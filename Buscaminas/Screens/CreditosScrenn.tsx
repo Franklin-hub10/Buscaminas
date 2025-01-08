c31430e651e19aeb787e04eb85e6268dd2dfec17
Isaac-TC <iseduardot92@gmail.com>
2025-01-07 23:20:33 -0500

    creditos

--- Buscaminas/Screens/CreditosScrenn.tsx
+++ Buscaminas/Screens/CreditosScrenn.tsx
@@ -1,10 +1,50 @@
-import { View, Text } from 'react-native'
+import { View, Text, StyleSheet } from 'react-native'
 import React from 'react'
 
-export default function CreditosScrenn() {
+export default function CreditosScreen() {
   return (
-    <View>
-      <Text>CreditosScrenn</Text>
+    <View style={styles.container}>
+      <View style={styles.textContainer}>
+        <Text style={styles.text}>Chritopher Aleman</Text>
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.text}>ChristopherDetken</Text>
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.text}>Franklin Polacin</Text>
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.text}>Franklin-hub10</Text>
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.text}>Isaac Tonato</Text>
+      </View>
+      <View style={styles.textContainer}>
+        <Text style={styles.text}>Isaac-TC</Text>
+      </View>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#000', // Fondo negro
+    padding: 20,
+  },
+  textContainer: {
+    backgroundColor: '#1c1c1c', // Fondo del contenedor ligeramente más claro
+    padding: 15,
+    marginVertical: 10,
+    borderRadius: 10,
+    width: '80%', // Ancho del contenedor
+    alignItems: 'center',
+  },
+  text: {
+    color: '#fff', // Letras blancas
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+})
