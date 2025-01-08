4f6682160a221c176f5125a53f7eab0341004fa9
Isaac-TC <iseduardot92@gmail.com>
2025-01-07 21:45:34 -0500

    firebase

--- Buscaminas/config/Config.tsx
+++ Buscaminas/config/Config.tsx
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 
 import { initializeApp } from "firebase/app";
+import { getDatabase} from 'firebase/database'
+
 
 // TODO: Add SDKs for Firebase products that you want to use
 
@@ -29,3 +31,4 @@ const firebaseConfig = {
 // Initialize Firebase
 
 const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app)
