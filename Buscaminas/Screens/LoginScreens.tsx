f9f9167ce3ef31bc03bf2096e32c171ec7873c0b
Isaac-TC <iseduardot92@gmail.com>
2025-01-10 20:51:34 -0500

    actualizacion

--- Buscaminas/Screens/LoginScreens.tsx
+++ Buscaminas/Screens/LoginScreens.tsx
@@ -1,76 +1,105 @@
-import { Button, StyleSheet, Text, TextInput, View, TouchableOpacity, Alert } from 'react-native';
-import React, { useEffect, useState } from 'react';
-import { child, get, ref } from "firebase/database";
-import { db } from '../config/Config';
-import { useNavigation, NavigationProp } from '@react-navigation/native';
-import { RootStackParams } from '../Browser/MainNavigator'; // Ajusta la ruta según tu proyecto
+import { Alert, Button, StyleSheet, Text, TextInput, View, TouchableOpacity, Modal } from 'react-native';
+import React, { useState } from 'react';
+import { getAuth, sendPasswordResetEmail, signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../config/Config';
 
-export default function LoginScreen() {
-    const navigation = useNavigation<NavigationProp<RootStackParams>>();
+export default function LoginScreen({ navigation }: any) {
+    const [correo, setcorreo] = useState('');
+    const [contrasenia, setContrasenia] = useState('');
+    const [ver, setver] = useState(false)
+    const [correoRestablecer, setcorreoRestablecer] = useState('')
 
-    const [email, setemail] = useState('');
-    const [password, setPassword] = useState('');
+    function login() {
+        signInWithEmailAndPassword(auth, correo, contrasenia)
+            .then((userCredential) => {
+                // Signed in
+                const user = userCredential.user;
+                navigation.navigate('Juego');
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                let titulo;
+                let mensaje;
 
-    const handleLogin = async () => {
-        if (!email || !password) {
-            Alert.alert('Error', 'Por favor, ingrese su correo y contraseña.');
-            return;
-        }
-
-        try {
-            // Referencia a la base de datos
-            const dbRef = ref(db);
-            const snapshot = await get(child(dbRef, 'usuarios'));
-            if (snapshot.exists()) {
-                const usuarios = snapshot.val();
-
-                // Buscar el usuario con email y password
-                const usuarioEncontrado = Object.values(usuarios).find(
-                    (usuario: any) => usuario.email === email && usuario.password === password
-                );
-
-                if (usuarioEncontrado) {
-                    Alert.alert('Éxito', 'Inicio de sesión exitoso.');
-                    navigation.navigate('Juego'); // Redirigir a la pantalla deseada
+                if (errorCode === 'auth/wrong-password') {
+                    titulo = 'Error en la contraseña';
+                    mensaje = 'Contraseña incorrecta. Por favor, verifica los datos ingresados.';
+                } else if (errorCode === 'auth/user-not-found') {
+                    titulo = 'Usuario no encontrado';
+                    mensaje = 'Por favor verifica el email ingresado.';
                 } else {
-                    Alert.alert('Error', 'Correo o contraseña incorrectos.');
+                    titulo = 'Error';
+                    mensaje = 'Verifica tus credenciales.';
                 }
-            } else {
-                Alert.alert('Error', 'No hay usuarios registrados.');
-            }
-        } catch (error) {
-            console.error(error);
-            Alert.alert('Error', 'Hubo un problema al validar el usuario.');
-        }
-    };
+
+                Alert.alert(titulo, mensaje);
+            });
+    }
+
+    function restablecer(){
+        const auth = getAuth();
+sendPasswordResetEmail(auth, correoRestablecer)
+  .then(() => {
+    // Password reset email sent!
+    // ..
+    Alert.alert('mensaje' , 'se a enviado correo ')
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+    Alert.alert(errorCode, errorMessage)
+  });
+
+    }
 
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Bienvenido</Text>
+            <Text style={styles.title}>Iniciar Sesión</Text>
+
             <TextInput
                 style={styles.input}
                 placeholder="Correo Electrónico"
                 placeholderTextColor="#aaa"
-                onChangeText={setemail}
-                value={email}
+                onChangeText={(texto) => setcorreo(texto)}
                 keyboardType="email-address"
                 autoCapitalize="none"
             />
+
             <TextInput
                 style={styles.input}
                 placeholder="Contraseña"
                 placeholderTextColor="#aaa"
-                onChangeText={setPassword}
-                value={password}
+                onChangeText={(texto) => setContrasenia(texto)}
                 secureTextEntry={true}
                 autoCapitalize="none"
             />
-            <TouchableOpacity style={styles.button} onPress={handleLogin}>
-                <Text style={styles.buttonText}>Acceder</Text>
+
+            <TouchableOpacity style={styles.button} onPress={login}>
+                <Text style={styles.buttonText}>Iniciar Sesión</Text>
             </TouchableOpacity>
-            <TouchableOpacity onPress={() => navigation.navigate('Botton')}>
-                <Text style={styles.registerText}>¿Eres nuevo? Regístrate aquí</Text>
+
+            <TouchableOpacity onPress={() => navigation.navigate('Registro')}>
+                <Text style={styles.registerText}>¿No tienes una cuenta? Regístrate aquí</Text>
             </TouchableOpacity>
+
+            
+            <TouchableOpacity onPress={() => setver (!ver)} >
+                <Text style={styles.registerText }>olvido la contrasenia. da click aqui</Text>
+            </TouchableOpacity>
+
+            <Modal visible= {ver} transparent={false}>
+                <View>
+                    <TextInput  style={styles.input}
+                    placeholder='ingresar correo'
+                    onChangeText={(texto)=> setcorreoRestablecer}
+                    />
+                    <Button  title='enviar' onPress={()=> restablecer ()} />
+                    <Button title='cerrar' onPress={()=> setver(!ver)} color={'red'}/>
+                </View>
+                
+            </Modal>
         </View>
     );
 }
@@ -80,34 +109,44 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: '#f0f0f0',
+        backgroundColor: '#f9f9f9',
         padding: 20,
     },
     title: {
-        fontSize: 32,
+        fontSize: 28,
         fontWeight: 'bold',
-        color: '#4CAF50',
-        marginBottom: 20,
+        color: '#333',
+        marginBottom: 30,
     },
     input: {
         height: 50,
-        fontSize: 18,
+        fontSize: 16,
         backgroundColor: '#ffffff',
         marginVertical: 10,
-        borderRadius: 10,
-        paddingHorizontal: 20,
+        borderRadius: 8,
+        paddingHorizontal: 15,
         width: '100%',
         borderWidth: 1,
-        borderColor: '#ccc',
+        borderColor: '#ddd',
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 5,
+        elevation: 2,
     },
     button: {
         marginTop: 20,
         backgroundColor: '#4CAF50',
-        borderRadius: 10,
+        borderRadius: 8,
         width: '100%',
         height: 50,
         justifyContent: 'center',
         alignItems: 'center',
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        shadowRadius: 3,
+        elevation: 3,
     },
     buttonText: {
         fontSize: 18,
@@ -115,7 +154,7 @@ const styles = StyleSheet.create({
         color: '#ffffff',
     },
     registerText: {
-        marginTop: 10,
+        marginTop: 15,
         fontSize: 16,
         color: '#4CAF50',
         textDecorationLine: 'underline',
